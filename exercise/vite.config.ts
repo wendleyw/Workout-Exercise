6a982be1367435d927d3c38f27cf8854159e3d82
wendleyw <wendleywilson@gmail.com>
2025-04-13 21:37:09 -0400

    Update vite.config.ts

--- exercise/vite.config.ts
+++ exercise/vite.config.ts
@@ -1,7 +1,21 @@
-typescriptimport { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: process.cwd(),
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
